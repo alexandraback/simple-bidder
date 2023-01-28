f7afd0399fe770991e4e3d9a95e5d667ca9805d2
pingsays <pingli88@gmail.com>
2023-01-28 16:41:46 -0500

    Enh/devcontainer (#2)
    * dev: enhanced devcontainer
    
    * test: fixed previously broken test after making changes to data model
    
    * dev: added port forwarding to devcontainer
    
    * dev: added vscode user and updated poetry install script

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,4 @@
-import ulid
-from simple_bidder.main import main
-from simple_bidder.models.campaign import Campaign
-
-
-# def test_main():
-#     print(main())
+from simple_bidder.models.campaign import CampaignResponse
 
 
 def test_campaign_with_id():
@@ -12,13 +6,16 @@ def test_campaign_with_id():
         "name": "Alex Campaign",
         "keywords": ["awesome programmer", "goat programmer"],
         "id": "1234",
+        "budget": 100,
+        "spending": 0,
     }
-    check = Campaign.parse_obj(check_expect)
+    check = CampaignResponse.parse_obj(check_expect)
     assert check == check_expect
 
 
 def test_campaign_no_id():
-    campaign = Campaign(
-        name="Alex Campaign", keywords=["awesome programmer", "goat programmer"]
+    campaign = CampaignResponse(
+        name="Alex Campaign",
+        keywords=["awesome programmer", "goat programmer"],
     )
     assert campaign.id is not None and len(campaign.id) == 26
